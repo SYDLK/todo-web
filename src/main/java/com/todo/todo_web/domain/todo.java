487488c670abb97cead7d9c483255f28e37b6fb4
SYDLK <kmj977284@gmail.com>
2025-06-06 03:07:14 +0900

    feat: create user domain structure

---
+++ src/main/java/com/todo/todo_web/domain/todo.java
@@ -0,0 +1,50 @@
+package com.todo.todo_web.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+
+public class todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "title", updatable = false)
+    private String title;
+
+    @Column(name = "content", updatable = false)
+    private String content;
+
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Builder
+    public todo(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
+
+    public void update(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
+}
---
+++ src/main/java/com/todo/todo_web/domain/user.java
@@ -0,0 +1,21 @@
+package com.todo.todo_web.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
+
+public class user {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
